23ea956e6059341cacb630815e7166f349b0a8a5
XiaoWu-5759 <1981293730@qq.com>
2019-06-20 17:37:57 +0800

    2019.06.20 追上最新的

---
+++ rabbitmq/src/main/java/com/xiaowu/rabbitmq/config/RabbitmqConfig.java
@@ -0,0 +1,15 @@
+package com.xiaowu.rabbitmq.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitmqConfig {
+    private static final Logger log = LoggerFactory.getLogger(RabbitmqConfig.class);
+
+    @Autowired
+    private Caching
+
+}
