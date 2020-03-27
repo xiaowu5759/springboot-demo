9d6a37ab72906b6cfc801638e84bf62219a67eeb
XiaoWu_5759 <1981293730@qq.com>
2020-03-27 15:34:39 +0800

    simba1060

--- springbootduty/src/main/java/com/xiaowu/springbootduty/service/Impl/CheckServiceImpl.java
+++
@@ -1,7 +0,0 @@
-package com.xiaowu.springbootduty.service.Impl;
-
-public interface CheckServiceImpl {
-
-    String check(String file);
-}
-
--- springbootduty/src/main/java/com/xiaowu/springbootduty/service/UserService.java
+++
@@ -1,9 +0,0 @@
-package com.xiaowu.springbootduty.service;
-
-import com.xiaowu.springbootduty.dao.User;
-
-public interface UserService {
-    User queryByName(String name);
-
-    User checkLogin(String name, String pwd);
-}
